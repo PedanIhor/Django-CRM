79f7d0b2a56b8c7cb725658d0aa6c3bf4ce6a49d
Ihor Pedan <ihorpedan@gmail.com>
2025-01-20 15:02:59 +0100

    Added a top bar with Add Role button

--- front-end/react-crm/src/pages/roles/Roles.tsx
+++ front-end/react-crm/src/pages/roles/Roles.tsx
@@ -1,11 +1,34 @@
 import React from 'react';
+import { Box, Button, Stack, } from '@mui/material'
+import { CustomToolbar } from '../../styles/CssStyled';
+import { FiPlus } from "@react-icons/all-files/fi/FiPlus";
 
-const Roles: React.FC = () => {
-  return (
-    <div>
-      <h1>Roles</h1>
-    </div>
-  );
+ export default function Roles() {
+
+    const onAddRole = () => {
+        // navigate('/app/roles/add-roles')
+    }
+
+    return (
+        <Box sx={{ mt: '60px' }}>
+            <CustomToolbar>
+                <Stack sx={{ 
+                    display: 'flex', 
+                    flexDirection: 'row', 
+                    alignItems: 'center',
+                    width: '100%',
+                    justifyContent: 'flex-end'
+                }}>
+                    <Button
+                        variant='contained'
+                        startIcon={<FiPlus className='plus-icon' />}
+                        onClick={onAddRole}
+                        className={'add-button'}
+                    >
+                        Add Role
+                    </Button>
+                </Stack>
+            </CustomToolbar>
+        </Box>
+    );
 };
-
-export default Roles;
